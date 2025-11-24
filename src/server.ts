2f740c76f927bac21a323206f223eb63912589d1
Tsvetan Tsaryanski <ttsaryanski@gmail.com>
2025-11-24 18:01:06 +0200

    Edit server

--- src/server.ts
+++ src/server.ts
@@ -6,12 +6,16 @@ const PORT = 3000;
 const port = process.env.PORT || PORT;
 
 async function startServer() {
+    app.listen(port, () =>
+        console.log(`Server running on http://localhost:${port}`)
+    );
+
     try {
+        console.log("Connecting to databases...");
+
         await Promise.all([mongooseInit(), postgresInit()]);
 
-        app.listen(port, () =>
-            console.log(`Server running on http://localhost:${port}`)
-        );
+        console.log("Databases initialized.");
     } catch (error) {
         console.log("Failed to initialize databases:", error);
         process.exit(1);
